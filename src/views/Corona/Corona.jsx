9c9eb6830af2c8f088c0c28f39dc267822a331b8
rsoghall <ryan@dksit.com>
2020-03-23 18:21:36 -0600

    Corona Page update

--- src/views/Corona/Corona.jsx
+++ src/views/Corona/Corona.jsx
@@ -6,7 +6,7 @@ export default function Corona() {
             <p>
                 At Dianne Adair we are continuing to monitor the current situation and daily changes that are
                 happening across our country and communities as efforts are made to slow down the spread of
-                COVID - 19. As of today Dianne Adair Day Care Centers will remain closed through Spring
+                COVID - 19. *As of today Dianne Adair Day Care Centers will remain closed through Spring
                 Break. Our current plan is to reopen on Monday April 13th. During this closure through April
                 10th, Dianne Adair will continue to pay all employees their normal wages. Parents will be billed
                 based on their contracted hours starting April 13th. Bills will not go out until we reopen.
@@ -15,10 +15,19 @@ export default function Corona() {
                 stress and hardship that many are going through with this unchartered territory. We hope that all
                 will remain healthy and strong through this pandemic.
             </p>
-            <text>
-                Brian Carbine <br />
-                Executive Director/CFO
-            </text>
+            <div className="Corona-bottom">
+                <div>
+                    Brian Carbine <br />
+                    Executive Director/CFO
+                </div>
+                <div>
+                    *Tice Creek: Open Now <br />
+                    *Bancroft: Will Open Monday, March 30th <br />
+                    *Pleasant Hill: Will Open Monday, March 30th
+                </div>
+            
+
+            </div>
         </div>
     )
 }
