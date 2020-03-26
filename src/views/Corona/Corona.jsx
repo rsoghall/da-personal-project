164599c1cda4c734550468f9e513b375bf442de2
rsoghall <ryan@dksit.com>
2020-03-26 16:31:48 -0600

    Corona Page update Reopen times and parent letter

--- src/views/Corona/Corona.jsx
+++ src/views/Corona/Corona.jsx
@@ -1,33 +1,104 @@
 import React from 'react'
 import './Corona.css'
+// import Reopen from './Reopen'
 export default function Corona() {
     return (
         <div className="Corona-Wrapper">
             <p>
-                At Dianne Adair we are continuing to monitor the current situation and daily changes that are
-                happening across our country and communities as efforts are made to slow down the spread of
-                COVID - 19. *As of today Dianne Adair Day Care Centers will remain closed through Spring
-                Break. Our current plan is to reopen on Monday April 13th. During this closure through April
-                10th, Dianne Adair will continue to pay all employees their normal wages. Parents will be billed
-                based on their contracted hours starting April 13th. Bills will not go out until we reopen.
-                Families should turn in their April contracts with a start date of April 13th. As we continue to
-                monitor the situation, we will inform everyone if our current plan changes. We understand the
-                stress and hardship that many are going through with this unchartered territory. We hope that all
-                will remain healthy and strong through this pandemic.
+                At Dianne Adair, we are continuing to monitor the current situation and daily changes
+                that are happening across our country and communities as efforts are made to slow down
+                the spread of COVID - 19. We initially closed all centers but have now slowly begun
+                reopening them. Please see the current scheduled dates for our centers to reopen below. During
+                these closures,  Dianne Adair will continue to pay all employees their normal wages.
+                Parents will be billed based on their contracted hours starting April 13th. Bills will
+                not go out until we reopen. Families should turn in their April contracts with a start
+                date of April 13th. As we continue to monitor the situation, we will inform everyone
+                if our current plan changes. We understand the stress and hardship that many are going
+                through with this unchartered territory. We hope that all will remain healthy and strong
+                through this pandemic.
             </p>
-            <div className="Corona-bottom">
+            <div className="Corona-sig">
+                Brian Carbine <br />
+                Executive Director/CFO
+            </div>
+            <h1 className="Corona-Title">
+                    Reopen Dates
+                </h1>
+            <div className="Corona-opening">
                 <div>
-                    Brian Carbine <br />
-                    Executive Director/CFO
+                    <h2>
+                        Open Now
+                    </h2>
+                    <div>
+                        Tice Creek  
+                    </div>
                 </div>
                 <div>
-                    *Tice Creek: Open Now <br />
-                    *Bancroft: Will Open Monday, March 30th <br />
-                    *Pleasant Hill: Will Open Monday, March 30th
+                    <h2>
+                        March 30th
+                    </h2>
+                    <div>
+                        Bancroft<br />
+                        Highlands<br />
+                        Pleasant Hill
+                    </div>
                 </div>
-            
+                <div>
+                    <h2>
+                        April 6th
+                    </h2>
+                    <div>
+                        Sequoia<br />
+                        Westwood<br />
+                    </div>
+                </div>
+                <div>
+                    <h2>
+                        April 13th
+                    </h2>
+                    <div>
+                        Ayers<br />
+                        Delta View<br />
+                        El Monte<br />
+                        Monte Gardens<br />
+                    </div>
+                </div>
 
             </div>
+            <h1 className="Corona-Title">
+                Letter from a Parent
+            </h1>
+            
+            <p> 
+                I would like to express my thank you, as a parent, to the staff at
+                Dianne Adair at Tice Creek for staying open during the coronavirus public
+                emergency. This is despite all of the extraordinary measures that they take
+                to clean the facility during this crisis while other childcare centers
+                shutter. My wife and I know that our 3 kids are well taken care of all day
+                with a few of their friends by caregivers with whom they are very familiar.
+                It is like a family there.
+            </p>
+            <p>
+                My wife is a manager at Kaiser Permanente's Walnut Creek Medical Center,
+                just down Newell Road.  Besides the direct management of medical
+                assistants and other medical staff facing an unprecedented health scare,
+                she also is involved in Kaiser's contingency planning for the pandemic. 
+                For the past several weeks, she has been working long hours and weekends
+                related to the health emergency. She is working late this evening. That
+                Dianne Adair at Tice Creek has been able to stay open has helped her
+                concentrate on serving this important purpose. While overlooked by some
+                of the public, this is precisely the intention of our public health
+                officers in keeping certain childcare centers open and functioning
+                (Contra Costa Health Services 3/16/2020 order, ¶10.f.xxi).
+            </p>
+            <p>
+                It is our hope that Dianne Adair remains open through the end of the
+                crisis and when schools finally reopen. Other parents in the health
+                care field who have children at Tice Creek should know that it is open.
+                We hope that you understand our appreciation for Dianne Adair's service
+                and, in their small way, helping our medical establishment deal with the
+                unprecedented crisis. 
+            </p>
         </div>
     )
 }
