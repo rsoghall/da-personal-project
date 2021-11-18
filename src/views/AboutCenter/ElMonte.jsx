1b89f6021ca38894bc64370309687c68132ecca0
rsoghall <ryan@dksit.com>
2021-11-17 19:27:00 -0700

    El Monte About Us - delete

--- src/views/AboutCenter/ElMonte.jsx
+++ src/views/AboutCenter/ElMonte.jsx
@@ -5,28 +5,16 @@ export default function ElMonte() {
     return (
         <div className="aboutCenter-wrapper">
             <p className="aboutCenter-ElMonte">
-                At Dianne Adair El Monte we understand that the single most 
-                important thing to parents is their children. Our number one 
-                priority is to provide a safe, positive experience for all the 
-                children in our care and to truly become a home away from home.
+                
             </p>
             <h4>
-                We offer:
+            
             </h4>
             <p  className="aboutCenter-ElMonte">
-                Before and after school care for TK-5th grade. Help with homework,
-                art, sports, science, cooking, field trips and much more! We have a
-                quality preschool program starting at age 2 years old. We teach Houghton
-                Mifflin and Handwriting Without Tears curriculum. We provide a
-                potty-training reward-based program for our little ones. 
-                We are open most holidays and non-school days including a very fun filled
-                summer program. Hourly, daily, weekly and drop-in rates available. 
-                Nutritional snacks are served daily. Our licensed and certified staff are
-                trained in first aid and CPR.
+               
             </p>
             <p className="aboutCenter-ElMonte">  
-                If you would like more information and to set up a site tour, please call
-                the center directly at 925-682-5060 and ask for Lisa Brady.
+                
             </p>
         </div>
     )
