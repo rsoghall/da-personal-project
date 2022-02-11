ffb83d269beb313e6724b48a23f67e925f92e794
rsoghall <ryan@dksit.com>
2022-02-11 09:52:08 -0700

    remove registration link

--- src/views/RegForms/RegForms.jsx
+++ src/views/RegForms/RegForms.jsx
@@ -4,8 +4,8 @@ export default function RegForms() {
   return (
     <div className="showForms">
         
-        <a href="https://dianne-adair-s3.s3.amazonaws.com/9a3e30b9-f8fc-4a56-a551-720f1d7f04c1-New-Family-Enrollment-Packet.pdf">New Family Enrollment Packet</a>
-        <a href="https://dianne-adair-s3.s3.amazonaws.com/3cb6a131-3dae-41b8-bdfa-b3809b6773fc-Returning-Families-Re-Enrollment-Packet.pdf">Returning Family Enrollment Packet</a>
+        {/* <a href="https://dianne-adair-s3.s3.amazonaws.com/9a3e30b9-f8fc-4a56-a551-720f1d7f04c1-New-Family-Enrollment-Packet.pdf">New Family Enrollment Packet</a> */}
+        {/* <a href="https://dianne-adair-s3.s3.amazonaws.com/3cb6a131-3dae-41b8-bdfa-b3809b6773fc-Returning-Families-Re-Enrollment-Packet.pdf">Returning Family Enrollment Packet</a> */}
 
     </div>
 )
